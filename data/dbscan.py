62dc3b34ecb3e21015b187029720679c79098789
jkwiatk1 <01142696@pw.edu.pl>
2023-12-31 16:22:36 +0100

    Compare my DBSCAN with Scikit

--- data/dbscan.py
+++ data/dbscan.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def dbscan(X, epsilon, min_samples):
     labels = np.zeros(X.shape[0], dtype=int)
     cluster_id = 0
@@ -11,19 +12,21 @@ def dbscan(X, epsilon, min_samples):
 
         neighbors = region_query(X, i, epsilon)
         if len(neighbors) < min_samples:
-            labels[i] = -1  # Oznacz jako szum (noise)
+            labels[i] = -1  # (noise)
         else:
             cluster_id += 1
             expand_cluster(X, labels, i, neighbors, cluster_id, epsilon, min_samples)
 
     return labels
 
+
 def region_query(X, point_idx, epsilon):
     neighbors = []
     for i in range(X.shape[0]):
-        if np.linalg.norm(X[point_idx] - X[i]) < epsilon:
+        if point_idx != i and np.linalg.norm(X[point_idx] - X[i]) <= epsilon:  # Count euclidean distance
             neighbors.append(i)
     return neighbors
+
 
 def expand_cluster(X, labels, point_idx, neighbors, cluster_id, epsilon, min_samples):
     labels[point_idx] = cluster_id
@@ -48,12 +51,21 @@ min_samples = 5
 labels = dbscan(X, epsilon, min_samples)
 
 plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
+plt.subplot(1, 3, 1)
 plt.scatter(X[:, 0], X[:, 1], c='gray', label='Dane przed DBSCAN')
 plt.title('Przed DBSCAN')
 
-plt.subplot(1, 2, 2)
+plt.subplot(1, 3, 2)
 plt.scatter(X[:, 0], X[:, 1], c=labels, cmap='viridis', label='Klastry po DBSCAN')
 plt.title('Po DBSCAN')
+
+from sklearn.cluster import DBSCAN
+
+dbscan_sk = DBSCAN(eps=epsilon, min_samples=min_samples)
+labels_sk = dbscan_sk.fit_predict(X)
+
+plt.subplot(1, 3, 3)
+plt.scatter(X[:, 0], X[:, 1], c=labels_sk, cmap='viridis', label='Klastry po DBSCAN scikit')
+plt.title('Po DBSCAN scikit')
 
 plt.show()
